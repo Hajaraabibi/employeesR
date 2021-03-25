13be97e6be896d2cd9397f86ac6415dc8ebd1411
hajara bibi <40460495+Hajaraabibi@users.noreply.github.com>
2021-03-25 10:28:29 +0000

    Add files via upload

---
+++ Employees.R
@@ -0,0 +1,8 @@
+employeeinfo = data.frame(Name=c("Kate Ware","Pamela Cisneros","Jaydan Cook","Marcel Smith","Niko Wheeler"),
+                          Age=c("32","56","23","34","36"),
+                          Gender=c("F","F","M","M","M"),
+                          Role=c("Manager","CEO","Administrator","Customer Service Advisor","Coach"),
+                          Timeofemployment=c("3 years","6 years","1 year","3 years","4 years")
+                          )
+print("information of employees:")
+print(employeeinfo)
